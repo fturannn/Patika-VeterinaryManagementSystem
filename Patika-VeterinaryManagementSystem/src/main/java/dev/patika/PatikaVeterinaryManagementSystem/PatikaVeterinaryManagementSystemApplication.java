2dc5183727411bcbcb3d071885426b80e59ee77d
fturannn <furkn.turan@hotmail.com>
2023-12-04 14:19:23 +0300

    initialization

---
+++ Patika-VeterinaryManagementSystem/src/main/java/dev/patika/PatikaVeterinaryManagementSystem/PatikaVeterinaryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.patika.PatikaVeterinaryManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatikaVeterinaryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatikaVeterinaryManagementSystemApplication.class, args);
+	}
+
+}
